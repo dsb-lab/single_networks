6a371e99aefca44d597982b06c49a93959448ea1
keithtopher <50483819+keithtopher@users.noreply.github.com>
2023-05-29 12:40:53 +0200

    Update index.js
    Add clickers to show neighbors and table for degree

--- js/index.js
+++ js/index.js
@@ -10,22 +10,9 @@
 
     let cy;
 
-    let $stylesheet = $('#style');
-    let getStylesheet = name => {
-      let convert = res => name.match(/[.]json$/) ? toJson(res) : toText(res);
-
-      return fetch(`stylesheets/${name}`).then( convert );
-    };
-    let applyStylesheet = stylesheet => {
-      if( typeof stylesheet === typeof '' ){
-        cy.style().fromString( stylesheet ).update();
-      } else {
-        cy.style().fromJson( stylesheet ).update();
-      }
-    };
-    let applyStylesheetFromSelect = () => Promise.resolve( $stylesheet.value ).then( getStylesheet ).then( applyStylesheet );
-
     let $dataset = $('#data');
+    console.log($dataset.value)
+
     let getDataset = name => fetch(`datasets/${name}`).then( toJson );
     let applyDataset = dataset => {
       // so new eles are offscreen
@@ -37,174 +24,135 @@
       cy.add( dataset );
     }
     let applyDatasetFromSelect = () => Promise.resolve( $dataset.value ).then( getDataset ).then( applyDataset );
+    // let applyDatasetFromSelect = () => Promise.resolve( dataFile ).then( getDataset ).then( applyDataset );
 
-    let calculateCachedCentrality = () => {
-      let nodes = cy.nodes();
-
-      if( nodes.length > 0 && nodes[0].data('centrality') == null ){
-        let centrality = cy.elements().closenessCentralityNormalized();
-
-        nodes.forEach( n => n.data( 'centrality', centrality.closeness(n) ) );
-      }
-    };
-
-    let $layout = $('#layout');
-    let maxLayoutDuration = 1500;
-    let layoutPadding = 50;
-    let concentric = function( node ){
-      calculateCachedCentrality();
-
-      return node.data('centrality');
-    };
-    let levelWidth = function( nodes ){
-      calculateCachedCentrality();
-
-      let min = nodes.min( n => n.data('centrality') ).value;
-      let max = nodes.max( n => n.data('centrality') ).value;
-
-
-      return ( max - min ) / 5;
-    };
-    let layouts = {
-      cola: {
-        name: 'cola',
-        padding: layoutPadding,
-        nodeSpacing: 12,
-        edgeLengthVal: 45,
-        animate: true,
-        randomize: true,
-        maxSimulationTime: maxLayoutDuration,
-        boundingBox: { // to give cola more space to resolve initial overlaps
-          x1: 0,
-          y1: 0,
-          x2: 10000,
-          y2: 10000
-        },
-        edgeLength: function( e ){
-          let w = e.data('weight');
-
-          if( w == null ){
-            w = 0.5;
-          }
-
-          return 45 / w;
-        }
-      },
-      concentricCentrality: {
-        name: 'concentric',
-        padding: layoutPadding,
-        animate: true,
-        animationDuration: maxLayoutDuration,
-        concentric: concentric,
-        levelWidth: levelWidth
-      },
-      concentricHierarchyCentrality: {
-        name: 'concentric',
-        padding: layoutPadding,
-        animate: true,
-        animationDuration: maxLayoutDuration,
-        concentric: concentric,
-        levelWidth: levelWidth,
-        sweep: Math.PI * 2 / 3,
-        clockwise: true,
-        startAngle: Math.PI * 1 / 6
-      },
-      custom: { // replace with your own layout parameters
-        name: 'preset',
-        padding: layoutPadding
-      }
-    };
-    let prevLayout;
-    let getLayout = name => Promise.resolve( layouts[ name ] );
-    let applyLayout = layout => {
-      if( prevLayout ){
-        prevLayout.stop();
-      }
-
-      let l = prevLayout = cy.makeLayout( layout );
-
-      return l.run().promiseOn('layoutstop');
-    }
-    let applyLayoutFromSelect = () => Promise.resolve( $layout.value ).then( getLayout ).then( applyLayout );
-
-    let $algorithm = $('#algorithm');
-    let getAlgorithm = (name) => {
-      switch (name) {
-        case 'bfs': return Promise.resolve(cy.elements().bfs.bind(cy.elements()));
-        case 'dfs': return Promise.resolve(cy.elements().dfs.bind(cy.elements()));
-        case 'astar': return Promise.resolve(cy.elements().aStar.bind(cy.elements()));
-        case 'none': return Promise.resolve(undefined);
-        case 'custom': return Promise.resolve(undefined); // replace with algorithm of choice
-        default: return Promise.resolve(undefined);
-      }
-    };
-    let runAlgorithm = (algorithm) => {
-      if (algorithm === undefined) {
-        return Promise.resolve(undefined);
+    // set preset stylesheet
+    styleName = 'custom.json'
+    let convert = res => styleName.match(/[.]json$/) ? toJson(res) : toText(res);
+    manualStyle = fetch(`stylesheets/${styleName}`).then( convert );
+    let applyStylesheet = stylesheet => {
+      if( typeof stylesheet === typeof '' ){
+        cy.style().fromString( stylesheet ).update();
       } else {
-        let options = {
-          root: '#' + cy.nodes()[0].id(),
-          // astar requires target; goal property is ignored for other algorithms
-          goal: '#' + cy.nodes()[Math.round(Math.random() * (cy.nodes().size() - 1))].id()
-        };
-        return Promise.resolve(algorithm(options));
-      }
-    }
-    let currentAlgorithm;
-    let animateAlgorithm = (algResults) => {
-      // clear old algorithm results
-      cy.$().removeClass('highlighted start end');
-      currentAlgorithm = algResults;
-      if (algResults === undefined || algResults.path === undefined) {
-        return Promise.resolve();
-      }
-      else {
-        let i = 0;
-        // for astar, highlight first and final before showing path
-        if (algResults.distance) {
-          // Among DFS, BFS, A*, only A* will have the distance property defined
-          algResults.path.first().addClass('highlighted start');
-          algResults.path.last().addClass('highlighted end');
-          // i is not advanced to 1, so start node is effectively highlighted twice.
-          // this is intentional; creates a short pause between highlighting ends and highlighting the path
-        }
-        return new Promise(resolve => {
-          let highlightNext = () => {
-            if (currentAlgorithm === algResults && i < algResults.path.length) {
-              algResults.path[i].addClass('highlighted');
-              i++;
-              setTimeout(highlightNext, 500);
-            } else {
-              // resolve when finished or when a new algorithm has started visualization
-              resolve();
-            }
-          }
-          highlightNext();
-        });
+        cy.style().fromJson( stylesheet ).update();
       }
     };
-    let applyAlgorithmFromSelect = () => Promise.resolve( $algorithm.value ).then( getAlgorithm ).then( runAlgorithm ).then( animateAlgorithm );
+    let applyStylesheetFromSelect = () => Promise.resolve( manualStyle ).then( applyStylesheet );
+
+    // get preset layout
+    let options = {name:'preset',padding:20}
+    let applyLayout = layout => {
+      return cy.makeLayout( layout ).run().promiseOn('layoutstop');
+    }
+    let applyLayoutFromSelect = () => Promise.resolve( options ).then( applyLayout );
 
     cy = window.cy = cytoscape({
       container: $('#cy')
     });
 
+
+    $dataset.addEventListener('change', function(){
+      tryPromise( applyDatasetFromSelect );
+    });
+
     tryPromise( applyDatasetFromSelect ).then( applyStylesheetFromSelect ).then( applyLayoutFromSelect );
 
     $dataset.addEventListener('change', function(){
-      tryPromise( applyDatasetFromSelect ).then( applyLayoutFromSelect ).then ( applyAlgorithmFromSelect );
+      tryPromise( applyDatasetFromSelect ).then( applyStylesheetFromSelect ).then( applyLayoutFromSelect );
     });
 
-    $stylesheet.addEventListener('change', applyStylesheetFromSelect);
+    // $stylesheet.addEventListener('change', applyStylesheetFromSelect);
 
-    $layout.addEventListener('change', applyLayoutFromSelect);
+    // Keith function for highlighting neighborhood
+    cy.on("click", "node", function(event) {
 
-    $('#redo-layout').addEventListener('click', applyLayoutFromSelect);
+      let clickNode = event.target
+      let connectedNodes = clickNode.neighborhood().nodes();
+      let remainingNodes = cy.nodes().not(connectedNodes);
+      let connectedEdges = clickNode.neighborhood().edges();
+      let remainingEdges = cy.edges().not(connectedEdges);
+      let nNodes = cy.nodes().length;
 
-    $algorithm.addEventListener('change', applyAlgorithmFromSelect);
 
-    $('#redo-algorithm').addEventListener('click', applyAlgorithmFromSelect);
+      // make the remaining nodes more transparent
+      remainingNodes.css({"border-color":"black",
+              "border-width":"2",
+              "opacity":"0.1"});
+
+      connectedNodes.css({"border-color":"cyan",
+              "border-width":"8",
+              "opacity":"1"});
+
+      clickNode.css(  {"border-color":"lime",
+                  "border-width": "8",
+                  "opacity":"1"});      
+
+
+      // highlight positive and negative edges and increase thickness
+      for (let i = 0; i < connectedEdges.length; i++){
+          edge = connectedEdges[i]
+          edge_thick = 3*edge.data("edge_thick")
+          edge_trans = edge.data("edge_trans")
+          weight = edge.data("weight")
+          if (weight < 0) { line_color = "red" } else if (weight > 0) {line_color = "blue"}
+          edge.css({"line-color":line_color,
+                    "width":edge_thick,
+                    "opacity":edge_trans}); // reset opacity from last click
+      }
+
+      // make the remaining edges more transparent
+      for (let i = 0; i < remainingEdges.length; i++) {
+          edge = remainingEdges[i]
+          edge_trans = 0.1*edge.data("edge_trans")
+          edge.css({"line-color":"black",
+                    "opacity":edge_trans});
+      }
+
+      nodeList = connectedNodes.map(x => x.id());
+      nodeStr = String(nodeList).replaceAll(',','\n');
+
+      edgeList = connectedEdges.map(x => [ x.data("source"),
+                                           x.data("target"), 
+                                           x.data("weight") ] );
+      for (let i = 0; i < edgeList.length; i++) {
+          let [source,target,weight] = edgeList[i]
+          if (source == clickNode.id()) {
+              edgeList[i] = [weight,target]
+          } else if (target == clickNode.id()) {
+              edgeList[i] = [weight,source]
+          }
+      }
+
+      edgeList = edgeList.sort( (a, b) => {return b[0] - a[0];} );
+      edgeStr = String(edgeList).replaceAll(',','\n');
+
+      console.log( "number of nodes " + nNodes + '\n'
+                  + "current node " + this.id() + '\n' 
+            			+ "degree  " + clickNode.data("degree") + '\n' );
+
+      tableText = '';
+      for (let i=0; i<edgeList.length; i++) {
+          let [weight,node] = edgeList[i]
+          weight = String(weight.toFixed(2))
+          tableText += `${weight.padStart(5)} : ${node.padEnd(20)} \n` // funky apostrophes needed
+      }
+
+      // add the text of the nodes and weights
+      htmlText = tableText.replaceAll('\n','<br>').replaceAll(' ','&nbsp;');
+      document.getElementById('networkInfo').innerHTML = htmlText;
+
+      // adjust dimensions of the box
+      topPad = document.getElementById("networkInfo").style["padding-top"].replace('px','')
+      lineHeight = document.getElementById("networkInfo").style["line-height"]
+      fontSize = document.getElementById("networkInfo").style["font-size"].replace('px','');
+      boxHeight = nodeList.length*parseFloat(lineHeight)*parseFloat(fontSize)+2*topPad
+      StrHeight = String( Math.floor( boxHeight ) ) + 'px';
+      document.getElementById("networkInfo").style.setProperty("height", StrHeight);
+      
+  	});
+	
   });
+
 })();
 
 // tooltips with jQuery
